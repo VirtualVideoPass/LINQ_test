5eb3f4763ba11f134fe105a2ab5ee23178a69e26
kaferi <kaferi.andrew@gmail.com>
2016-08-17 18:52:59 +0300

    ch 1

--- LINQtest/LINQtest/Program.cs
+++ LINQtest/LINQtest/Program.cs
@@ -8,12 +8,25 @@ namespace LINQtest
 {
     class Program
     {
+
+        //delegate Func<string, int> selector();
+
+        private static int selector(string s)
+        {
+            return Int32.Parse(s);
+        }
+
         static void Main(string[] args)
         {
             string[] numbers = { "40", "2012", "176", "5" };
 
+
+
             // Преобразуем массив строк в массив типа int используя LINQ
-            int[] nums = numbers.Select(s => Int32.Parse(s)).ToArray();
+            //int[] nums = numbers.Select(s => Int32.Parse(s)).ToArray();
+
+            
+            int[] nums = numbers.Select(selector).ToArray();
 
             foreach (int n in nums)
                 Console.Write(n + " ");
