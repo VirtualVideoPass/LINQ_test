216ace8c1b388453ceb88349ece25ca36a550a91
kaferi <kaferi.andrew@gmail.com>
2016-08-17 18:29:21 +0300

    init

---
+++ LINQtest/LINQtest/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LINQtest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] numbers = { "40", "2012", "176", "5" };
+
+            // Преобразуем массив строк в массив типа int используя LINQ
+            int[] nums = numbers.Select(s => Int32.Parse(s)).ToArray();
+
+            foreach (int n in nums)
+                Console.Write(n + " ");
+
+        }
+    }
+}
